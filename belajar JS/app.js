5e641424e04e375026a6141ce262d0c907d49ae8
Irwan12337 <is0841832@gmail.com>
2024-11-12 14:44:11 +0700

    java script

---
+++ belajar JS/app.js
@@ -0,0 +1,59 @@
+//console.log('hello world');
+
+// if (1 + 2 === 3) {
+//     console.log('Hitungan benar');
+// }
+
+// let nomor = Math.random()
+// console.log(nomor);
+
+// if (nomor >= 0.5) {
+//     console.log('angka ini lebih besar dari pembanding 0.5')
+// }
+
+
+// if (nomor <= 0.5) {
+//     console.log('angka ini lebih kecil dari pembanding 0.5')
+// }
+
+// didalam else if kutipan dalam konsol log pastikan menggunakan "", karena kalau '' tidak jalan
+
+// const nilai = prompt ('isi nilai');
+
+// if (nilai < 50) {
+//     console.log("F Tolol")
+// }else if (nilai < 60) {
+//     console.log ("D Belajarlagi")
+// }else if (nilai < 70) {
+//     console.log("C cukup")
+// }else if (nilai < 80) {
+//     console.log ("B baik")
+// }else if (nilai < 90 ){
+//     console.log ("B+ bagus sekali")
+// }else if (nilai < 100) {
+//     console.log("A Super")
+// }else if (nilai == 100) {
+//     console.log("Sempurna")
+// }
+
+
+// ELSE
+
+const iniHari = prompt('isikan hari');
+if (iniHari === 'senin') {
+    console.log ('oh shit');
+}else if (iniHari === 'sabtu') {
+    console.log ('yey liburan bermula');
+}else if (iniHari === 'minggu') {
+    console.log ('yap sehari lagi');
+}else if (iniHari === 'selasa') {
+    console.log ('yah biasa aja');
+}else if (iniHari === 'rabu') {
+    console.log ('yah biasa aja');
+}else if (iniHari === 'kamis') {
+    console.log ('yah biasa aja');
+}else if (iniHari === 'jumat') {
+    console.log ('Kita berada di puncak seluncuran');
+}else {
+    console.log ('Itu bukan hari laa');
+}
