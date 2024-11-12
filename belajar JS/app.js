727507378e3c544a8fcde3715d7ec0ef790d1af6
Irwan12337 <is0841832@gmail.com>
2024-11-13 00:13:13 +0700

    belajar logika js

--- belajar JS/app.js
+++ belajar JS/app.js
@@ -39,21 +39,61 @@
 
 // ELSE
 
-const iniHari = prompt('isikan hari');
-if (iniHari === 'senin') {
-    console.log ('oh shit');
-}else if (iniHari === 'sabtu') {
-    console.log ('yey liburan bermula');
-}else if (iniHari === 'minggu') {
-    console.log ('yap sehari lagi');
-}else if (iniHari === 'selasa') {
-    console.log ('yah biasa aja');
-}else if (iniHari === 'rabu') {
-    console.log ('yah biasa aja');
-}else if (iniHari === 'kamis') {
-    console.log ('yah biasa aja');
-}else if (iniHari === 'jumat') {
-    console.log ('Kita berada di puncak seluncuran');
+// const iniHari = prompt('isikan hari');
+// if (iniHari === 'senin') {
+//     console.log ('oh shit');
+// }else if (iniHari === 'sabtu') {
+//     console.log ('yey liburan bermula');
+// }else if (iniHari === 'minggu') {
+//     console.log ('yap sehari lagi');
+// }else if (iniHari === 'selasa') {
+//     console.log ('yah biasa aja');
+// }else if (iniHari === 'rabu') {
+//     console.log ('yah biasa aja');
+// }else if (iniHari === 'kamis') {
+//     console.log ('yah biasa aja');
+// }else if (iniHari === 'jumat') {
+//     console.log ('Kita berada di puncak seluncuran');
+// }else {
+//     console.log ('Itu bukan hari laa');
+// }
+
+// IF didalam IF
+
+// const password = prompt ("Buat kata sandinya");
+
+// if(password.length >= 6) {
+//     if(password.indexOf(' ') ==-1) {
+//         console.log("password valid")
+//     }else {
+//         console.log("password tidak boleh ada spasi")
+//     }
+// }else {
+//     console.log("Password minimal 6 karakter")
+// }
+
+//Logika ANd &&
+
+// const password = prompt ("Buat kata sandinya");
+// if(password.length >= 6 && password.indexOf(' ') ===-1) {
+//     console.log ("password valid")
+// }else {
+//     console.log("password tidak memenuhi kriteria")
+// }
+
+
+//Logika OR ||
+// const role = prompt("masuk sebagai?");
+// if(role === 'admin' || role === 'spv') {
+//     console.log("Akses diterima")
+// }else {
+//     console.log("Maaf, akses di tolak")
+// }
+
+//Logika Not !
+const role = prompt("masuk sebagai?");
+if (role !== 'admin') {
+    console.log('Dilarang mengakses')
 }else {
-    console.log ('Itu bukan hari laa');
+    console.log("Akses diterima")
 }
